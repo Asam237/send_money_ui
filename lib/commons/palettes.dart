1595ff086210696c7e324a147d746a64805fec8e
Asam237 <abbasalimokolo@gmail.com>
2022-11-16 08:23:16 +0100

    feat: add palette

---
+++ lib/commons/palettes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Palettes {
+  static MaterialColor primary = const MaterialColor(0xff000000, <int, Color>{
+    50: Color(0xffe7eefb),
+  });
+  static MaterialColor secondary = const MaterialColor(0xff6834F5, <int, Color>{
+    50: Color(0xffe7eefb),
+  });
+}
